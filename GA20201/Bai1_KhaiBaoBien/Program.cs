3651da55ceb0462cbd251694f101be4e89e47c40
Phúc <93722448+PhucNood@users.noreply.github.com>
2024-09-14 10:24:12 +0700

    Bài 1 + Bài 2

---
+++ GA20201/Bai1_KhaiBaoBien/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace Bai1_KhaiBaoBien
+{
+    internal class Program
+    {
+        /*
+        * Bài 1.1 Khai báo biến
+        * Định nghĩa biến: Dùng để lưu trữ giá trị hoặc 1 tập giá trị và biến được khởi tạo trong bộ nhớ ảo khi chương trình khởi chạy.
+        *
+        * Công thức: <Kiểu dữ liệu> <Tên Biến> = <Giá trị>;
+        *      Khai báo nhiều biến có cùng kiểu dữ liệu trên cùng 1 dòng:
+        *      <Kiểu dữ liệu> <Tên Biến1> = <Giá trị>, <Tên Biến2> = <Giá trị>, <tên biến n>;
+        * 1. <Kiểu dữ liệu> 
+        *  + Số nguyên:sbyte, byte, long, int
+        *  + Sô thực: float, double, decimal
+        *  + Số nguyên không có dấu (dương) : uint, ushort, ulong
+        *  + Chuỗi hoặc ký tự: string, char
+        *  + Logic: bool
+        *  +object: kiểu dữ liệu cha của mọi kiểu dữ liệu, có thể biến đổi thành các kiểu dữ liệu khác qua unboxing
+        *  + var: Kiểu khai báo ngầm định, kiểu dữ liệu được xác định theo kiểu dữ liệu khởi tạo (gán)
+        * 2. <Tên Biến>
+        *  + Quy tắc:
+        *      - Tên biến phải có nghĩa
+        *      - Có phân biệt chữ hoa chữ thường
+        *      - Không bắt đầu bởi số, không dùng từ khóa
+        *      - Cố gắng làm quen đặt tên biến với Tiếng Anh
+        *  + Khai báo tên biến với 2 từ trở lên:
+        *      - Cách 1: normal = firstname
+        *      - Cách 2: CamelCase = FirstName, firstName
+        *      - Cách 3: Under_Score = first_name, first_Name (hay còn gọi là snake_case)
+        *  + Biến cục bộ: là biến được khai báo bên trong 1 hàm.
+        *  + Biến toàn cục: là biến được khai báo bên ngoài hàm.
+        *      - Khi khai báo biến toàn cục sử dụng dấu gạch dưới _ trước tên biến.
+        *      - int _year = 1989;
+        * 3.<Giá trị>
+        *  + Khi khai báo biến có thể khởi tạo giá trị hoặc không khởi tạo giá trị.
+        *  + Giá trị phải đúng các quy tắc với kiểu dữ liệu
+        *
+        */
+        //biến toàn cục
+        static int _namSinh = 2006;
+        static void Main(string[] args)
+        {
+            //gõ được tiếng việt
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            //biến cục bộ
+            int a;
+            int b = 5;
+
+            double diem = 8.2;
+            float chieuCao = 152, can;
+            uint nam_sinh = 2004;
+           
+
+            char c = 'H'; // 1 ký tự
+            //char[] hoTen;
+            string hoTen = "Peter Pan";
+
+            bool isBoy = true; //true- nam, false- nữ
+
+
+            var soNguyen = 50; // Gán là kiểu số nguyên  => int
+            var soThuc = 3.5; // Khởi tạo là số thực => double
+            object logic = false;
+            // object có thể đại diện cho mọi kiểu dữ liệu, nhưng nên hạn chế sử dụng vì
+            // sẽ giảm hiệu xuất và tính rõ ràng
+          
+            // Các cách để chỉnh format in ra 
+            Console.WriteLine(hoTen); // cách viết nhanh Console.WriteLine() => cw Tab
+            Console.WriteLine("Tên tôi là: " + hoTen + " cao: " + chieuCao);  // Sử dụng dấu +
+            Console.WriteLine($"Tên tôi là: {hoTen} cao {chieuCao}"); // Thêm $ rồi truyền các biến vào string
+            Console.WriteLine("Tuổi là: {0}",DateTime.Now.Year - nam_sinh); // Sử dụng Formater của string
+           
+        }
+
+    }
+}
---
+++ GA20201/Bai2_Input_ChuyenDoiKieuDuLieu/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Runtime;
+using System.Text;
+
+namespace Bai2_Input_ChuyenDoiKieuDuLieu
+{
+    internal class Program
+    {
+        /*
+        * P1: Ép kiểu - Chuyển đổi từ kiểu dữ liệu số sang dạng số khác - Không đổ kiểu dữ liệu lớn về bé mà chỉ bé lên lớn ví dụ int lớn hơn byte hoặc long nhất trong số nguyên. Ngoài ra các bạn có thể tham khảo lại trong bảng kích thước kiểu dữ liệu.
+        *
+        * Công thức ép kiểu tường minh:
+        *        (<kiểu dữ liệu>) <biến cần ép kiểu>;
+        *
+        * P2: 
+        * Chuyển đổi từ string về số
+        *  + Phương thức Parse()
+        *  + Lớp Convert()
+        * Chuyển đổi từ kiểu số về string
+        * Sử dụng phương thức toString hoặc + ""
+        * 
+        * P3: Chuyển kiểu giá trị sang tham chiếu và ngược lại
+        * +Boxing: Giá trị sang tham chiếu
+        * +Unboxing: tham chiếu sang giá trị
+        */
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //PHẦN 1: ÉP KIỂU GIỮA CÁC SỐ
+            //ép kiểu ngầm định
+            int a = 12;
+            double b = a;
+            //cw tab
+            Console.WriteLine($"a= {a}, kiểu của a: {a.GetType().Name}");
+            Console.WriteLine($"b= {b}, kiểu của b: {b.GetType().Name} ");
+
+            //ép kiểu tường minh (casting)
+            double c = 9.3;
+            int d = (int)c;
+            Console.WriteLine($"c= {c}");
+            Console.WriteLine($"d= {d}");
+
+            //PHẦN 2: ÉP KIỂU từ STRING về SỐ
+
+            //Để đọc 1 chuỗi các ký tự từ bàn phím ta sử dụng Console.ReadLine()
+            Console.WriteLine("Xin mời nhập: ");
+            string input = Console.ReadLine(); // Kiểu trả về sau khi thực hiện câu lệnh sẽ là string
+            Console.WriteLine("Bạn vừa nhập {0} từ bàn phím!",input);
+            string number1 = "8";
+            string number2 = "10";
+            Console.WriteLine(number1 + number2); // kết quả là 810 thay vì 18 vì đây là string
+
+            //int.Parse => nếu string null => ArgumentNullException:
+            int n1 = int.Parse(number1); 
+            //Convert.ToInt32 => nếu string là null 
+            //=> nhận giá trị mặc định của kiểu dữ liệu vd =0
+            int n2 = Convert.ToInt32(number2); // Int32 tương đương với int (32bit)
+
+            Console.WriteLine(n1+n2); // kết quả là 18 vì là kiểu số
+
+            //Đổi từ số sang string 
+            string s1 = n1.ToString();
+            string s2 = n2 + "";
+
+            Console.WriteLine(s1+s2); //810 vì là kiểu strubg
+            /*            *P3: Chuyển kiểu giá trị sang tham chiếu và ngược lại
+             *            
+             *            Giá trị sang tham chiếu
+            */
+            int giaTri = -92; // Kiểu giá trị
+            object thamChieu = giaTri; // Kiểu tham chiếu
+
+            // Tham chiếu sang giá trị
+            object thamChieu1 = 123;// kiểu tham chiếu
+            uint giaTri1 = (uint)thamChieu1; // Kiểu giá trị
+
+        
+        }
+    }
+}
