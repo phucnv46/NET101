5c995682afa1a0b68b635f87940783ef744f4bb2
Phúc <93722448+PhucNood@users.noreply.github.com>
2024-09-17 09:15:13 +0700

    Bài 3 + 4 : Điều Kiện + Vòng lặp

---
+++ GA20201/Bai3_CauLenhDieuKien_ReNhanh/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Text;
+
+namespace Bai3_CauLenhDieuKien_ReNhanh
+{
+    internal class Program
+    {
+        /*
+         P1: Cấu trúc điều kiện (if -else if -else) 
+         == : So sánh bằng (bằng nhau) VD: 2 == 4:2
+         != : So sánh khác (không bằng ) VD: 3 != 4
+        > : Lớn hơn  VD 6 > 3 (true)
+        < : nhỏ hơn VD 9 < 6 (false)
+         >= : Lớn hơn  VD 6 >= 3 (true)
+        <= : nhỏ hơn hoặc bằng VD 9 < 6 (false)
+        && : true && true => true, true && false => false
+        ||: true && false => true, ...
+         Toán tử 3 ngôi
+         P2: Cấu trúc rẽ nhánh (switch -case)
+         
+         */
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            /*int dayOfWeek = 5;
+            if (dayOfWeek > 2)
+            {
+                Console.WriteLine("Monday");
+            }if (dayOfWeek > 4)
+            {
+                Console.WriteLine("Wesday");
+            }
+            else if (dayOfWeek > 3) // Khi mà có 1 điều kiện thỏa mãn sẽ không xét các điều kiên khác nữa
+            {
+                Console.WriteLine("Tuesday");
+            }
+            if(dayOfWeek > 2 && dayOfWeek <6)
+            {
+                Console.WriteLine("Else");
+            }
+
+            bool gender = false;  // true cho nam, false cho nu
+            if (gender!=true)
+            {
+                Console.WriteLine("Nữ");
+            }
+            else
+            {
+                Console.WriteLine("Nam");
+            }
+            // Toán tử 3 ngôi:
+            string sex = gender == true ? "Nam" : "Nữ";
+            Console.WriteLine(sex);*/
+
+
+            // Bài tập
+            // Viết điều kiện cho tài - xỉu 
+            // xỉu <=9  (3-9)  
+            // tài >=10 (10-18)
+
+            TaiXiu();
+
+            // Bài 2:
+            // Khai báo 1 số kiểm tra số đấy có là số lẻ hay chẵn ( 3 ngôi )
+            // % chai lấy dư
+            // số chẵn % 2 == 0
+
+            ChanLe();
+
+            //P2 : Cấu trúc rẽ nhánh
+
+            ChuyenKenh();
+
+            //Bài tập
+            /*
+             * In ra 1 cái menu lựa chọn đồ uống Trà đá, nước vối, me , ...
+             * Viết switch case in ra lựa chọn đồ uống
+             * */
+            Menu();
+
+            
+
+        }
+
+        static void TaiXiu()
+        {
+
+            Console.WriteLine("Xin mời nhập tổng điểm:");
+            uint tongDiem = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Bạn vừa nhập {0}", tongDiem);
+
+            if (tongDiem < 3 || tongDiem > 18)
+            {
+                return;
+            }
+            if (tongDiem >= 3 && tongDiem < 10)
+            {
+                Console.WriteLine("Xỉu");
+            }
+            else
+            {
+                Console.WriteLine("Tài");
+            }
+        }
+
+        static void ChanLe()
+        {
+            Console.Write("Xin mời nhập số: ");
+            uint num = Convert.ToUInt32(Console.ReadLine());
+            string chanLe = num % 2 == 0 ? "Chẵn" : "Lẻ";
+            Console.WriteLine($"Số này là số {chanLe}");
+        }
+
+        static void ChuyenKenh()
+        {
+            Console.Write("Xin mời nhập kênh: ");
+            uint kenh = Convert.ToUInt32(Console.ReadLine());
+            switch (kenh)
+            {
+                case 1:
+                    Console.WriteLine("VTV1");// nếu chọn 1 sẽ đi đến VTV 1
+                    break;
+                case 2:
+                    Console.WriteLine("VTV2");
+                    break;
+                case 3:
+                    Console.WriteLine("VTV3");
+                    break;
+                default:
+                    Console.WriteLine("Không bắt được sóng");
+                    break;
+            }
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("----Menu---- \n 1.Trà đá \n 2.Nhân trần \n 3.La Hán \n 0.Exit ");
+            Console.Write("Xin mời lựa chọn: ");
+            uint doUong = Convert.ToUInt32(Console.ReadLine());
+
+            switch (doUong)
+            {
+                case 1: Console.WriteLine("Trà đá"); break;
+                case 2: Console.WriteLine("Nhân trần"); break;
+                case 3: Console.WriteLine("La Hán"); break;
+                case 0: return;
+                default: Console.WriteLine("Không có trong menu"); break;
+
+            }
+        }
+    }
+
+}
+
+
---
+++ GA20201/Bai4_VongLap/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace Bai4_VongLap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //i++ == i=i+1
+            /*for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine(i+1);
+            }*/
+
+            /*  int num = 0;
+              while (num < 30)
+              {
+                  num++;
+                  Console.WriteLine(num);
+              }
+  */
+            //do-while
+            /* int num = 0;
+             do
+             {
+                 num++;
+                 Console.WriteLine(num);
+             } while (num <30);*/
+
+            // Dung lap in ra menu 
+
+            for (;;)
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("----Menu---- \n 1.Trà đá \n 2.Nhân trần \n 3.La Hán \n 0.Exit ");
+                    Console.Write("Xin mời lựa chọn: ");
+                    uint doUong = Convert.ToUInt32(Console.ReadLine());
+
+                    switch (doUong)
+                    {
+                        case 1: Console.WriteLine("Trà đá"); break;
+                        case 2: Console.WriteLine("Nhân trần"); break;
+                        case 3: Console.WriteLine("La Hán"); break;
+                        case 0: return;
+                        default: Console.WriteLine("Không có trong menu"); break;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Chỉ được nhập số!");
+                }
+            }
+
+            //Bt: Viết menu cho TaiXiu, Chuyen Kenh, ChanLe, TraDa
+
+
+        }
+    }
+}
