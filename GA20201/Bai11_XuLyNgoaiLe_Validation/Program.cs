e294de330636eb16aa8cc19296bf7d8ea373a98e
Phúc <93722448+PhucNood@users.noreply.github.com>
2024-10-08 08:12:01 +0700

    upload bài 11 Validation

---
+++ GA20201/Bai11_XuLyNgoaiLe_Validation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace Bai11_XuLyNgoaiLe_Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            /*int a = Validation.CheckInt("Xin mời nhập số nguyên:",int.MinValue, int.MaxValue);*/
+            string email = Validation.CheckEmail("Xin mời nhập email: ");
+        }
+    }
+}
---
+++ GA20201/Bai11_XuLyNgoaiLe_Validation/Validation.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Bai11_XuLyNgoaiLe_Validation
+{
+    internal class Validation
+    {
+        public static int CheckInt(string mess,int min, int max) // Kiểm tra đầu vào của 1 chuỗi có phải số nguyên không
+        {
+            while (true)
+            {
+                int result = 0;
+                Console.Write(mess);
+                string input = Console.ReadLine();
+                try
+                {
+                    if (string.IsNullOrEmpty(input)) // nếu nhập vào trống
+                    {
+                        Console.WriteLine("Nhập vào trống");
+                    }
+                    result = Convert.ToInt32(input);
+                    if(result < min || result > max) // Kiểm soát cận trên, cận dưới
+                    {
+                        Console.WriteLine("Số nhập vào phải nhỏ hơn {0} và lớn hơn {1}", max, min);
+                    }
+                    else
+                    {
+                        return result;
+                    }
+
+                   
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Xảy ra lỗi {0}", e.Message);
+
+                }
+            }
+           
+        }
+
+
+        public static double CheckDouble(string mess, double min, double max) // Kiểm tra đầu vào của 1 chuỗi có phải số nguyên không
+        {
+            while (true)
+            {
+                double result = 0;
+                Console.Write(mess);
+                string input = Console.ReadLine();
+                try
+                {
+                    if (string.IsNullOrEmpty(input)) // nếu nhập vào trống
+                    {
+                        Console.WriteLine("Nhập vào trống");
+                    }
+                    result = Convert.ToDouble(input);
+                    if (result < min || result > max) // Kiểm soát cận trên, cận dưới
+                    {
+                        Console.WriteLine("Số nhập vào phải nhỏ hơn {0} và lớn hơn {1}", max, min);
+                    }
+                    else
+                    {
+                        return result;
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Xảy ra lỗi {0}", e.Message);
+
+                }
+            }  
+
+        }
+
+        public static string CheckEmail(string mess) // Kiểm tra nhập vào có phải email không
+        {
+            
+            Regex emailPartend = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"); // quy định mẫu string phải nhập vào
+            while (true)
+            {
+                Console.Write(mess);
+                string input = Console.ReadLine();
+                if (emailPartend.IsMatch(input))
+                {
+                    Console.WriteLine("Email đúng mẫu");
+                    return input;
+                }
+                Console.WriteLine("Chưa đúng mẫu email!");
+            }
+        }
+    }
+}
